7666ffbd879ebeeec9807221e6a8810fe21eff13
RomanyshynOlga <robota.buh.lb@gmail.com>
2021-12-13 10:37:58 +0200

    #HW Errors fixed

--- homeworks/#Homework Romanyshyn Olga/#HW4.py
+++ homeworks/#Homework Romanyshyn Olga/#HW4.py
@@ -1,4 +1,4 @@
-#1. Create a Vehicle class with max_speed and mileage instance attributes
+# 1. Create a Vehicle class with max_speed and mileage instance attributes
 class Vehicle:
     car_type = 'my car'
 
@@ -7,8 +7,7 @@ class Vehicle:
         self.mileage = mileage
 
 
-
-#2. Create a child class Bus that will inherit all the variables and methods of the Vehicle class and will have seating_capacity own method
+# 2. Create a child class Bus that will inherit all the variables and methods of the Vehicle class and will have seating_capacity own method
 
 class Vehicle:
 
@@ -20,33 +19,34 @@ class Vehicle:
         print(f'Max speed is {self.max_speed}')
 
     def print_mileage(self):
-        print(f'All mileage is{self.mileage}')
+        print(f'All mileage is {self.mileage}')
+
+
 my_car = Vehicle(250, 25)
 my_car.print_mileage()
 my_car.print_max_speed()
-
 
 
 class Bus(Vehicle):
     def __init__(self, max_speed, mileage, seating_capacity):
         super().__init__(max_speed, mileage)
         self.seating_capacity = seating_capacity
+
     def print_bus(self):
-     print(f'My bus have{self.seating_capacity} seating')
+        print(f'My bus have{self.seating_capacity} seating')
+
 
 my_car = Bus(250, 150, 25)
 my_car.print_bus()
 
-#All mileage is25
-#Max speed is 250
-#My bus have25 seating
+# All mileage is25
+# Max speed is 250
+# My bus have25 seating
 
 
 # 3. Determine which class a given Bus object belongs to (Check type of an object)
 print(type(Bus))
-#<class 'type'>
-
-
+# <class 'type'>
 
 
 # 4. Determine if School_bus is also an instance of the Vehicle class
@@ -55,9 +55,7 @@ school_bus = Bus(100, 50, 5)
 print(isinstance(school_bus, Vehicle))
 
 
-
-
-#5. Create a new class School with get_school_id and number_of_students instance attributes
+# 5. Create a new class School with get_school_id and number_of_students instance attributes
 
 class School:
 
@@ -66,22 +64,22 @@ class School:
         self.number_of_stude = number_of_stude
 
 
-
-#6*. Create a new class SchoolBus that will inherit all the methods from School and Bus and will have its own - bus_school_color
+# 6*. Create a new class SchoolBus that will inherit all the methods from School and Bus and will have its own - bus_school_color
 
 class SchoolBus(Bus, School):
 
     def __init__(self, max_speed, mileage, seating_capacity, get_school_id, number_of_stude, bus_school_color):
         Bus.__init__(self, max_speed, mileage, seating_capacity)
         School.__init__(self, get_school_id, number_of_stude)
-        self.bus_shool_color)=color
+        self.bus_school_color = bus_school_color
+
     def bus_school_color(self):
-        print(f'It is {self.bus_school_color()}')
+        print(f'It is {self.bus_school_color}')
 
 
-#7. Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method. Create two instances, one of Bear and one of Wolf,
-make a tuple of it and by using for call their action using the same method.
-Magic methods:
+# 7. Polymorphism: Create two classes: Bear, Wolf. Both of them should have make_sound method. Create two instances, one of Bear and one of Wolf,
+
+
 
 class Bear:
     type_Bear = "Polar_bear"
@@ -109,5 +107,3 @@ animals = (Polar_bear, Dog)
 
 for animal_sound in animals:
     animal_sound.make_sound()
-
-
